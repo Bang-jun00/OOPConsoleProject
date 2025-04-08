7ff579483330f059b50dfe6f6de3f4513aa0dfe2
Bang-jun00 <qudwns5250@naver.com>
2025-04-08 09:51:48 +0900

    Init Visual Studio
    비주얼 스튜디오 프로젝트 생성

---
+++ 안병준_OOPConsolerProject/안병준_OOPConsolerProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace 안병준_OOPConsolerProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
