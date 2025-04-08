65d64bfe091a64e2a98904a41263ab2a3678c1ad
Bang-jun00 <qudwns5250@naver.com>
2025-04-08 19:19:29 +0900

    [Feat]Player 객체 구현
    열거형으로 직업의 종류 열거, 플레이어의 닉네임, 직업 타입, 직업 타입 마다의 체력 공격력 구현,

---
+++ 안병준_OOPConsolerProject/안병준_OOPConsolerProject/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 안병준_OOPConsolerProject
+{
+    public enum ClassType
+    {
+        None = 0,
+        Warrior = 1,
+        Mage = 2
+    }
+
+
+
+
+
+    public class Player
+    {
+        public string Nickname { get; set; }
+        public ClassType CharacterClass { get; set; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+
+        public Player(string nickname, ClassType characterClass)
+        {
+            Nickname = nickname;
+            CharacterClass = characterClass;
+            Setstats();
+        }
+
+
+        private void Setstats()
+        {
+            switch(CharacterClass)
+            {
+                case ClassType.Warrior:
+                    Health = 150;
+                    AttackPower = 30;
+                    break;
+                case ClassType.Mage:
+                    Health = 100;
+                    AttackPower = 50;
+                    break;
+            }
+        }
+    }
+}
+
+
