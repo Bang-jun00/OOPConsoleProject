a656693a883baf2c601c3a132bf4a43c78edcaf5
Bang-jun00 <qudwns5250@naver.com>
2025-04-09 10:16:00 +0900

    [Feat]위치 설정
    Vector2 구조체를 만들어 위치 설정

---
+++ 안병준_OOPConsolerProject/안병준_OOPConsolerProject/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 안병준_OOPConsolerProject
+{
+    public struct Vector2
+    {
+        public int x;
+        public int y;
+
+        public Vector2(int x, int y) 
+        { 
+            this.x = x; 
+            this.y = y; 
+        }
+    }
+         
+}
