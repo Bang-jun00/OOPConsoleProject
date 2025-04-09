582237bbdb90807d214e4379daef29d5d48addbd
Bang-jun00 <qudwns5250@naver.com>
2025-04-09 10:15:05 +0900

    [Feat]여러 씬 설정 및 일부 구현
    Field 씬에 맵이 출력되게 구현 일부 나머지 필드 구성만 추가

---
+++ 안병준_OOPConsolerProject/안병준_OOPConsolerProject/Scenes/FieldScene.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 안병준_OOPConsolerProject.Scenes
+{
+    public class FieldScene : Scene
+    {
+        private ConsoleKey key;
+        private string[] mapData;
+        private bool[,] map;
+        
+        public FieldScene()
+        {
+            mapData = new string[]
+            {
+            "############################################################",
+            "#               A    #             ## ##                   #",
+            "#  ##               #             ## C ##                  #",
+            "#  ##              #             ##     ##                 #",
+            "#   ## # # # # #  #             ##       ##                #",
+            "#   ##                          ##       ##      ###########",
+            "#   ##                           ##     ##       ##   L   ##",
+            "#   ##                                           ##       ##",
+            "#   ##                                           ######   ##",
+            "#   ##                  ##############                     #",
+            "#   ##                  ##    E     ##                     #",
+            "#   ##                  ##          ##                     #",
+            "#   ##                  #####    #####                     #",
+            "#     #     #                                              #",
+            "#      #   # #                                             #", 
+            "#       # #   #   #                                        #",
+            "#        #     # # #                                       #",
+            "#                   #                                      #",
+            "#              K     #                                   ★ #",
+            "############################################################"
+            };
+
+
+
+            map = new bool[20,60];
+
+            for(int y =0; y < map.GetLength(0); y++)
+            {
+                for(int x = 0; x < map.GetLength(1); x++)
+                {
+                    map[y,x] = mapData[y][x] == '#'? false : true;
+                }
+            }
+
+        }
+        
+        public override void Render()
+        {
+            PrintMap();
+            Console.WriteLine("E : Stage 1, L : Stage 2 , C : Stage 3, A : stage 4, K:Fianal Stage");
+        }
+
+        public override void Input()
+        {
+            key = Console.ReadKey(true).Key;
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void Result()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PrintMap()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                for (int x = 0; x < map.GetLength(1); x++)
+                {
+                    if(map[y,x] == true)
+                    {
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        Console.Write('#');
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ 안병준_OOPConsolerProject/안병준_OOPConsolerProject/Scenes/SanctuaryEidos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 안병준_OOPConsolerProject.Scenes
+{
+    public class SanctuaryEidos : Scene
+    {
+        public override void Render()
+        {
+            Print("이 아름다운 곳은 성역 에이도스");
+            Print("영웅을 찾는 여정을 시작하기 전 당신은 이곳에서 대비를 해야한다.\n");
+
+            Console.WriteLine("어떤 대비를 하시겠습니까?");
+            Print(" 1. 성역의 도서관에서 노아에 대한 기록을 찾는다.");
+            Print(" 2. 성역의 훈련장에서 전투훈련을 한다.");
+            Print(" 3. 성역의 소문난 맛집에서 든든하게 배를 채운다.\n");
+
+            Console.Write("선택 :   ");
+            string choice = Console.ReadLine();
+
+        }
+         
+
+        public override void Input()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Result()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
