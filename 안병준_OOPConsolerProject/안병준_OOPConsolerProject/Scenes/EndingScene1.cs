d5d02bf1422b3d1b8a6e8596f648134d1008f2b6
Bang-jun00 <qudwns5250@naver.com>
2025-04-09 11:35:19 +0900

    [Feat]EndingScene
    엔딩씬 1,2 구성 추가

---
+++ 안병준_OOPConsolerProject/안병준_OOPConsolerProject/Scenes/EndingScene1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 안병준_OOPConsolerProject.Scenes
+{
+    public class EndingScene1 : Scene
+    {
+        public override void Render()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Input()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Result()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
