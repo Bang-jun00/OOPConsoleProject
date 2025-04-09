51f8f8ad03343284d9837eb4194d2413834ba55c
Bang-jun00 <qudwns5250@naver.com>
2025-04-09 21:17:13 +0900

    [Feat]상호작용 기능
    FieldMap에 있는 Map과 상호작용 가능한 기능을 구현

---
+++ 안병준_OOPConsolerProject/안병준_OOPConsolerProject/GameObjPotal/Light.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 안병준_OOPConsolerProject.GameObjPotal
+{
+    public class Light : GameObject
+    {
+        private string scene;
+
+        public Light(string scene, char symbol, Vector2 position) : base(ConsoleColor.Yellow, symbol, position)
+        {
+            this.scene = scene;
+        }
+
+        public override void Interact(Player player)
+        {
+            Game.ChangeScene(scene);
+        }
+    }
+}
