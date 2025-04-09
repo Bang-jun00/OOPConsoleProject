a47c78cecaff4269dbe5da97cc82930061c7c807
Bang-jun00 <qudwns5250@naver.com>
2025-04-09 10:13:40 +0900

    [Feat] Player class
    움직임의 주체의 기능을 만드려고 Player class 새로 생성

--- 안병준_OOPConsolerProject/안병준_OOPConsolerProject/Player.cs
+++ 안병준_OOPConsolerProject/안병준_OOPConsolerProject/Player.cs
@@ -6,52 +6,7 @@ using System.Threading.Tasks;
 
 namespace 안병준_OOPConsolerProject
 {
-    public enum ClassType
+    class Player
     {
-        None = 0,
-        Warrior = 1,
-        Mage = 2
-    }
-
-
-
-
-
-    public class Player
-    {
-        public string Nickname { get; set; }
-        public ClassType CharacterClass { get; set; }
-        public int Health { get; set; }
-        public int AttackPower { get; set; }
-
-        public int Intelligent {  get; set; }
-
-        public Player(string nickname, ClassType characterClass)
-        {
-            Nickname = nickname;
-            CharacterClass = characterClass;
-            Setstats();
-        }
-
-
-        private void Setstats()
-        {
-            switch(CharacterClass)
-            {
-                case ClassType.Warrior:
-                    Health = 150;
-                    AttackPower = 30;
-                    Intelligent = 15;
-                    
-                    break;
-                case ClassType.Mage:
-                    Health = 100;
-                    AttackPower = 10;
-                    Intelligent = 35;
-                    break;
-            }
-        }
     }
 }
-
-
